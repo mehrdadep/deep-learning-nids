920050b2d56198cc8b2ee970852c62eb6a089aa4
mehrdadep <mehrdad.smaeilpour@yahoo.com>
2019-01-22 11:14:54 +0330

    add MinimalRNNCell

---
+++ MinimalRNNCell.py
@@ -0,0 +1,25 @@
+import keras
+
+
+class MinimalRNNCell(keras.layers.Layer):
+
+    def __init__(self, units, **kwargs):
+        self.units = units
+        self.state_size = units
+        super(MinimalRNNCell, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.kernel = self.add_weight(shape=(input_shape[-1], self.units),
+                                      initializer='uniform',
+                                      name='kernel')
+        self.recurrent_kernel = self.add_weight(
+            shape=(self.units, self.units),
+            initializer='uniform',
+            name='recurrent_kernel')
+        self.built = True
+
+    def call(self, inputs, states):
+        prev_output = states[0]
+        h = K.dot(inputs, self.kernel)
+        output = h + K.dot(prev_output, self.recurrent_kernel)
+        return output, [output]
--- rnn.py
+++ rnn.py
@@ -1,7 +1,16 @@
 from DataProccess import DataProccess
+from MinimalRNNCell import MinimalRNNCell
+import keras
+from keras import RNN
 
 data = DataProccess()
 
 data = data.return_proccessed_data()
 
-print(data[0][0])
+cell = MinimalRNNCell(32)
+x = keras.Input((None, 5))
+layer = RNN(cell)
+y = layer(x)
+
+keras.layers.SimpleRNN(units, activation='tanh', use_bias=True, kernel_initializer='glorot_uniform', recurrent_initializer='orthogonal', bias_initializer='zeros', kernel_regularizer=None, recurrent_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, recurrent_constraint=None, bias_constraint=None, dropout=0.0, recurrent_dropout=0.0, return_sequences=False, return_state=False, go_backwards=False, stateful=False, unroll=False)
+

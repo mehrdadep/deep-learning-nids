cf7a5ae996d56cff7ce533612099cb3c157b1965
mehrdadep <mehrdad.smaeilpour@yahoo.com>
2019-01-22 09:49:06 +0330

    create DataProccess class and a new file for dl model

---
+++ DataProccess.py
@@ -0,0 +1,215 @@
+import os
+import io
+from numpy import float64, array,amax,amin
+
+class DataProccess:
+        _DATA_FOLDER = "data"
+        @classmethod
+        def get_current_working_directory(self):
+                """
+                get current directory
+                """
+                return os.getcwd()
+        
+        @classmethod
+        def add_to_file(self,content, filename):
+                """
+                add content to filename in current directory
+                """
+                filepath = os.path.join(self.get_current_working_directory(), filename)
+
+                try:
+                        if not os.path.exists(filepath):
+                                with open(filepath, 'w'):
+                                        pass
+                                with open(filepath, "a") as f:
+                                        f.write(content+'\n')
+                                return True
+                except Exception:
+                        return False
+        
+        @classmethod
+        def read_file(self,filename):
+                """
+                        read a whole file as one with file name, not full path
+                """
+                filepath = os.path.join(self.get_current_working_directory(), self._DATA_FOLDER,filename)
+                with open(filepath, 'r', encoding='utf-8') as content:
+                        return content.read()
+        
+        @classmethod
+        def read_file_lines(self,filename):
+                """
+                read all lines of file with file name, not full path
+                """
+                filepath = os.path.join(self.get_current_working_directory(), 'data',filename)
+                with open(filepath, 'r', encoding='utf-8') as content:
+                        return content.readlines()
+        
+        @classmethod
+        def extract_features(self,a_line):
+                """
+                extract features based on comma (,), return an array
+                """
+                return [x.strip() for x in a_line.split(',')]
+       
+        @classmethod
+        def numericalize_feature(self,feature,protocol_type,service,flag):
+                protocol_type_count = len(protocol_type)
+                service_count = len(service)
+                flag_count = len(flag)
+
+                second_index = int(protocol_type_count+1)
+                third_index = int(protocol_type_count+service_count+1)
+                forth_index = int(protocol_type_count+service_count+flag_count+1)
+
+                # index 1 is protocol_type
+                feature[1:1] = protocol_type[feature[1]]
+                feature.pop(second_index)
+
+                # index 2 + protocol_type_count is service
+                feature[second_index:second_index] = service[feature[second_index]]
+                feature.pop(third_index)
+                # # index 3 + protocol_type_count + service_count is flag
+                feature[third_index:third_index] = flag[feature[third_index]]
+                feature.pop(forth_index)
+
+                # make all values float64
+                feature = [float64(x) for x in feature]
+
+                return array(feature)
+
+        @classmethod
+        def numericalize_result(self,reslut,attack):
+
+                second_index = int(len(attack))
+                # index 0 is attack
+                reslut[0:0] = attack[reslut[0]]
+                reslut.pop(second_index)
+                # make all values float64
+                reslut = [float64(x) for x in reslut]
+
+                return array(reslut)
+
+        @classmethod
+        def normalize_value(self,value, min, max):
+                value = float64(value)
+                min = float64(min)
+                max = float64(max)
+
+                if min == float64(0) and max == float64(0):
+                        return float64(0)
+                result = float64( (value - min) / (max - min) )
+                return result
+        
+        @classmethod
+        def return_proccessed_data(self):
+                """
+                read from data folder and return a list
+                [train_data, train_results, test_data, test_results]
+                """
+                train_data = self.read_file_lines('KDDTrain+.txt')
+                test_data = self.read_file_lines('KDDTest+.txt')
+
+                # create arrays of arrays from lines
+                raw_train_data_features = [self.extract_features(x) for x in train_data]
+                raw_test_data_features = [self.extract_features(x) for x in test_data]
+
+                # train data: put index 0 to 40 in data, 41 and 42 into result (we don't need 41,42 for now)
+                raw_train_data_results = [x[41:43] for x in raw_train_data_features]
+                raw_train_data_features = [x[0:41] for x in raw_train_data_features]
+
+                # test data: put index 0 to 40 in data, 41 and 42 into result (we don't need 41,42 for now)
+                raw_test_data_results = [x[41:43] for x in raw_test_data_features]
+                raw_test_data_features = [x[0:41] for x in raw_test_data_features]
+
+                # stage 1 : numericalization --> index 1, 2 and 3 of dataset
+                # 1.1 extract all protocol_types, services and flags
+                protocol_type = dict()
+                service = dict()
+                flag = dict()
+                attack = dict()
+
+                for entry in raw_train_data_features:
+                        protocol_type[entry[1]] = ""
+                        service[entry[2]] = ""
+                        flag[entry[3]] = ""
+
+                for entry in raw_test_data_features:
+                        protocol_type[entry[1]] = ""
+                        service[entry[2]] = ""
+                        flag[entry[3]] = ""
+
+                for entry in raw_train_data_results:
+                        attack[entry[0]] = ""
+
+                for entry in raw_test_data_results:
+                        attack[entry[0]] = ""
+
+                keys= list(protocol_type.keys())
+                for i in range(0,len(keys)):
+                        protocol_type[keys[i]] = [int(d) for d in str(bin(i)[2:].zfill(len(protocol_type)))]
+
+                keys= list(service.keys())
+                for i in range(0,len(keys)):
+                        service[keys[i]] = [int(d) for d in str(bin(i)[2:].zfill(len(service)))]
+
+                keys= list(flag.keys())
+                for i in range(0,len(keys)):
+                        flag[keys[i]] = [int(d) for d in str(bin(i)[2:].zfill(len(flag)))]
+
+
+                keys= list(attack.keys())
+                for i in range(0,len(keys)):
+                        attack[keys[i]] = [int(d) for d in str(bin(i)[2:].zfill(len(attack)))]
+
+                # train data
+                numericalized_train_data_features = [self.numericalize_feature(x,protocol_type,service,flag) for x in raw_train_data_features]
+                normalized_train_data_features = array(numericalized_train_data_features)
+
+                numericalized_train_data_results = [self.numericalize_result(x,attack) for x in raw_train_data_results]
+                normalized_train_data_results = array(numericalized_train_data_results)
+
+                # test data
+                numericalized_test_data_features = [self.numericalize_feature(x,protocol_type,service,flag) for x in raw_test_data_features]
+                normalized_test_data_features = array(numericalized_test_data_features)
+
+                numericalized_test_data_results = [self.numericalize_result(x,attack) for x in raw_test_data_results]
+                normalized_test_data_results = array(numericalized_test_data_results)
+
+                # stage 2: normalization --> x = (x - MIN) / (MAX - MIN) --> based on columns
+
+                # train data
+                ymin_train = amin(numericalized_train_data_features,axis=0)
+                ymax_train = amax(numericalized_train_data_features,axis=0)
+
+                ymin_train_result = amin(numericalized_train_data_features,axis=0)
+                ymax_train_result  = amax(numericalized_train_data_features,axis=0)
+
+                # test data
+                ymin_test = amin(numericalized_test_data_features,axis=0)
+                ymax_test = amax(numericalized_test_data_features,axis=0)
+
+                ymin_test_result = amin(numericalized_test_data_features,axis=0)
+                ymax_test_result = amax(numericalized_test_data_features,axis=0)
+
+                # normalize train
+                for x in range(0, normalized_train_data_features.shape[0]):
+                        for y in range(0, normalized_train_data_features.shape[1]):
+                                normalized_train_data_features[x][y] = self.normalize_value(normalized_train_data_features[x][y],ymin_train[y],ymax_train[y])
+
+                for x in range(0, normalized_train_data_results.shape[0]):
+                        for y in range(0, normalized_train_data_results.shape[1]):
+                                normalized_train_data_results[x][y] = self.normalize_value(normalized_train_data_results[x][y],ymin_train_result[y],ymax_train_result[y])
+
+                # normalize test
+                for x in range(0, normalized_test_data_features.shape[0]):
+                        for y in range(0, normalized_test_data_features.shape[1]):
+                                normalized_test_data_features[x][y] = self.normalize_value(normalized_test_data_features[x][y],ymin_test[y],ymax_test[y])
+
+                for x in range(0, normalized_test_data_results.shape[0]):
+                        for y in range(0, normalized_test_data_results.shape[1]):
+                                normalized_test_data_results[x][y] = self.normalize_value(normalized_test_data_results[x][y],ymin_test_result[y],ymax_test_result[y])
+                
+                return [normalized_train_data_features,normalized_train_data_results,normalized_test_data_features,normalized_test_data_results]
+        
---
+++ rnn.py
@@ -0,0 +1,7 @@
+from DataProccess import DataProccess
+
+data = DataProccess()
+
+data = data.return_proccessed_data()
+
+print(data[0][0])

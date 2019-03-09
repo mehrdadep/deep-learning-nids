dc53a2ea77d4191ccffb76dd95f1716acc3c2fdc
mehrdadep <esmaeilpour.mehrdad@gmail.com>
2019-03-09 09:51:09 +0330

    multiclass test cicids

--- lstm.py
+++ lstm.py
@@ -12,8 +12,8 @@ import winsound
 
 # get and process data
 data = DataProcess()
-# x_train, y_train, x_test, y_test = data.return_processed_cicids_data_multiclass()
-x_train, y_train, x_test, y_test = data.return_processed_cicids_data_binary()
+x_train, y_train, x_test, y_test = data.return_processed_cicids_data_multiclass()
+# x_train, y_train, x_test, y_test = data.return_processed_cicids_data_binary()
 
 
 # reshape input to be [samples, timesteps, features]
@@ -21,8 +21,8 @@ x_train = x_train.reshape(x_train.shape[0], 1, x_train.shape[1])
 x_test = x_test.reshape(x_test.shape[0], 1, x_test.shape[1])
 
 # multiclass
-# y_train=np_utils.to_categorical(y_train)
-# y_test=np_utils.to_categorical(y_test)
+y_train=np_utils.to_categorical(y_train)
+y_test=np_utils.to_categorical(y_test)
 
 model = Sequential()
 model.add(LSTM(120, input_shape = (x_train.shape[1],x_train.shape[2]), return_sequences=True))
@@ -39,8 +39,8 @@ model.add(Dense(1))
 model.add(Activation('sigmoid'))
 
 # multiclass
-# model.add(Dense(5))
-# model.add(Activation('softmax'))
+model.add(Dense(7))
+model.add(Activation('softmax'))
 
 model.summary()
 
@@ -48,13 +48,13 @@ model.summary()
 adam = Adam(lr=0.0001)
 
 #binary
-model.compile(optimizer = adam, loss = 'binary_crossentropy', metrics=['accuracy'])
+# model.compile(optimizer = adam, loss = 'binary_crossentropy', metrics=['accuracy'])
 
 #multiclass
-# model.compile(optimizer = adam, loss = 'categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer = adam, loss = 'categorical_crossentropy', metrics=['accuracy'])
 
 start = time.time()
-model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=30, batch_size=32)
+model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=5, batch_size=64)
 
 # save the model
 # model.save("model.hdf5")
@@ -69,9 +69,9 @@ y_pred = [np.round(x) for x in y_pred]
 y_pred = np.array(y_pred)
 print('Confusion Matrix')
 #binary
-print(confusion_matrix(y_test, y_pred))
+# print(confusion_matrix(y_test, y_pred))
 #multiclass
-# print(confusion_matrix(y_test.argmax(axis=1), y_pred.argmax(axis=1)))
+print(confusion_matrix(y_test.argmax(axis=1), y_pred.argmax(axis=1)))
 print('Classification Report')
 print(classification_report(y_test, y_pred))
 winsound.PlaySound("SystemHand", winsound.SND_ALIAS)
--- rnn.py
+++ rnn.py
@@ -7,22 +7,25 @@ import numpy as np
 from keras.utils import np_utils
 import time
 from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.utils import class_weight
 import winsound
 
 
 # get and process data
 data = DataProcess()
-# x_train, y_train, x_test, y_test = data.return_processed_cicids_data_multiclass()
-x_train, y_train, x_test, y_test = data.return_processed_cicids_data_binary()
-
+x_train, y_train, x_test, y_test = data.return_processed_cicids_data_multiclass()
+# x_train, y_train, x_test, y_test = data.return_processed_cicids_data_binary()
+# class_weights = class_weight.compute_class_weight('balanced',
+#                                                  np.unique(y_train),
+#                                                  y_train)
 
 # reshape input to be [samples, timesteps, features]
 x_train = x_train.reshape(x_train.shape[0], 1, x_train.shape[1])
 x_test = x_test.reshape(x_test.shape[0], 1, x_test.shape[1])
 
 # multiclass
-# y_train=np_utils.to_categorical(y_train)
-# y_test=np_utils.to_categorical(y_test)
+y_train=np_utils.to_categorical(y_train)
+y_test=np_utils.to_categorical(y_test)
 
 model = Sequential()
 model.add(SimpleRNN(120, input_shape = (x_train.shape[1],x_train.shape[2]), return_sequences=True))
@@ -35,12 +38,12 @@ model.add(SimpleRNN(120, return_sequences=False))
 model.add(Dropout(0.2))
 
 # binary
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
+# model.add(Dense(1))
+# model.add(Activation('sigmoid'))
 
 # multiclass
-# model.add(Dense(5))
-# model.add(Activation('softmax'))
+model.add(Dense(7))
+model.add(Activation('softmax'))
 
 model.summary()
 
@@ -48,13 +51,13 @@ model.summary()
 adam = Adam(lr=0.0001)
 
 #binary
-model.compile(optimizer = adam, loss = 'binary_crossentropy', metrics=['accuracy'])
+# model.compile(optimizer = adam, loss = 'binary_crossentropy', metrics=['accuracy'])
 
 #multiclass
-# model.compile(optimizer = adam, loss = 'categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer = adam, loss = 'categorical_crossentropy', metrics=['accuracy'])
 
 start = time.time()
-model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=30, batch_size=32)
+model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=5, batch_size=64)
 
 # save the model
 # model.save("model.hdf5")
@@ -72,9 +75,9 @@ y_pred = [np.round(x) for x in y_pred]
 y_pred = np.array(y_pred)
 print('Confusion Matrix')
 #binary
-print(confusion_matrix(y_test, y_pred))
+# print(confusion_matrix(y_test, y_pred))
 #multiclass
-# print(confusion_matrix(y_test.argmax(axis=1), y_pred.argmax(axis=1)))
+print(confusion_matrix(y_test.argmax(axis=1), y_pred.argmax(axis=1)))
 print('Classification Report')
 print(classification_report(y_test, y_pred))
 winsound.PlaySound("SystemHand", winsound.SND_ALIAS)
